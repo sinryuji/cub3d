d615a5e4a4e00fcfcb77d5601fdd2b01070fd4c5
kanghyki <kanghyki@gmail.com>
2022-12-10 21:17:08 +0900

    refactor: draw_texture()

--- src/raycast_02.c
+++ src/raycast_02.c
@@ -6,17 +6,18 @@
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 21:44:07 by hyeongki          #+#    #+#             */
-/*   Updated: 2022/12/09 18:40:29 by kanghyki         ###   ########.fr       */
+/*   Updated: 2022/12/10 21:16:10 by kanghyki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+int	get_texture_color(t_info *info, int index);
+
 void	calc_texture(t_info *info, int x)
 {
 	int	y;
-	int color;
-	int index;
+	int	index;
 
 	y = 0;
 	while (y < HEIGHT)
@@ -26,15 +27,7 @@ void	calc_texture(t_info *info, int x)
 			info->draw.tex_y = (int)info->draw.tex_pos & (TEX_HEIGHT - 1);
 			info->draw.tex_pos += info->draw.step;
 			index = info->draw.tex_y * TEX_WIDTH + info->draw.tex_x;
-			if (info->raycast.side == 1 && info->raycast.step_y == 1)
-				color = info->texture[NORTH][index];
-			else if (info->raycast.side == 1 && info->raycast.step_y == -1)
-				color = info->texture[SOUTH][index];
-			else if (info->raycast.side == 0 && info->raycast.step_x == 1)
-				color = info->texture[EAST][index];
-			else if (info->raycast.side == 0 && info->raycast.step_x == -1)
-				color = info->texture[WEST][index];
-			info->buf[y][x] = color;
+			info->buf[y][x] = get_texture_color(info, index);
 		}	
 		else if (y < info->draw.draw_start)
 			info->buf[y][x] = CEILING_COLOR;
@@ -42,4 +35,20 @@ void	calc_texture(t_info *info, int x)
 			info->buf[y][x] = FLOOR_COLOR;
 		++y;
 	}
+}
+
+int	get_texture_color(t_info *info, int index)
+{
+	int	ret;
+
+	ret = 0;
+	if (info->raycast.side == 1 && info->raycast.step_y == 1)
+		ret = info->texture[NORTH][index];
+	else if (info->raycast.side == 1 && info->raycast.step_y == -1)
+		ret = info->texture[SOUTH][index];
+	else if (info->raycast.side == 0 && info->raycast.step_x == 1)
+		ret = info->texture[EAST][index];
+	else if (info->raycast.side == 0 && info->raycast.step_x == -1)
+		ret = info->texture[WEST][index];
+	return (ret);
 }
